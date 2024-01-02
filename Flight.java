d0b19d27b8f6fe726de3d8db893739b222894e1b
Hmaza Hashmi <146145658+1Hamza-Hashmi1@users.noreply.github.com>
2024-01-02 07:51:26 -0500

    Update Flight.java

--- Flight.java
+++ Flight.java
@@ -1,14 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package lab1;
-
-/**
- *
- * @author Hamza
- */
 public class Flight {
     private int flightNumber;
     private String origin;
