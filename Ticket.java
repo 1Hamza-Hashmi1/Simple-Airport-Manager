599c67ef2c02200f5b2f8c931beff48e15ce87b1
Hmaza Hashmi <146145658+1Hamza-Hashmi1@users.noreply.github.com>
2024-01-02 07:54:21 -0500

    Update Ticket.java

--- Ticket.java
+++ Ticket.java
@@ -1,14 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package lab1;
-
-/**
- *
- * @author hamza
- */
 public class Ticket {
     private Passenger passenger;
     private Flight flight;
